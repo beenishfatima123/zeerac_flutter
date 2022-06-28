5c459aba88a64eb32cc391428e6e83eee1487384
zohaib1313 <mianzohaibifikhar@gmail.com>
2022-06-28 11:48:48 +0500

    video scoring complete

--- lib/modules/users/controllers/video_player_controller.dart
+++ lib/modules/users/controllers/video_player_controller.dart
@@ -1,15 +1,17 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:video_player/video_player.dart';
 import 'package:zeerac_flutter/dio_networking/app_apis.dart';
 import 'package:zeerac_flutter/modules/users/models/tutorial_response_model.dart';
+import 'package:zeerac_flutter/utils/helpers.dart';
 
 class VideoPlayerScoringController extends GetxController {
   RxBool isLoading = false.obs;
-  RxInt scores = (-1).obs;
+  int scores = 0;
+  RxBool isAnswerSubmitted = false.obs;
   late VideoPlayerController videoController;
 
-  RxList<Rx<Map<String, String>>> selectedAnswerList =
-      <Rx<Map<String, String>>>[].obs;
+  RxList<RxString> selectedAnswerList = <RxString>[].obs;
 
   void initializeVideo(TutorialResponseModel model) {
     videoController = VideoPlayerController.network((model.courseVideo ?? '')
@@ -18,7 +20,18 @@ class VideoPlayerScoringController extends GetxController {
         : 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4');
   }
 
-  void calculateScores() {
-    for (var element in selectedAnswerList) {}
+  void calculateScores(TutorialResponseModel model) {
+    for (var i = 0; i < (model.questions?.length ?? 0); i++) {
+      if (selectedAnswerList[i].value ==
+          (model.questions![i].correctAnswer ?? '')) {
+        scores++;
+      }
+    }
+  }
+
+  void resetQuiz() {
+    selectedAnswerList.clear();
+    scores = (0);
+    isAnswerSubmitted.value = false;
   }
 }
--- lib/modules/users/pages/tutorials/video_player_scoring_page.dart
+++ lib/modules/users/pages/tutorials/video_player_scoring_page.dart
@@ -11,6 +11,7 @@ import 'package:zeerac_flutter/modules/users/controllers/tutorials_controller.da
 import 'package:zeerac_flutter/modules/users/models/tutorial_response_model.dart';
 import 'package:zeerac_flutter/modules/users/pages/tutorials/tutorials_widget.dart';
 import 'package:zeerac_flutter/utils/app_alert_bottom_sheet.dart';
+import 'package:zeerac_flutter/utils/helpers.dart';
 import '../../../../common/loading_widget.dart';
 import '../../controllers/video_player_controller.dart';
 
@@ -24,6 +25,7 @@ class VideoPlayerScoringPage extends GetView<VideoPlayerScoringController>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: myAppBar(goBack: true, title: 'Tutorial'),
       body: GetX<VideoPlayerScoringController>(
         initState: (state) {
           if (model != null) {
@@ -84,23 +86,63 @@ class VideoPlayerScoringPage extends GetView<VideoPlayerScoringController>
                           ),
                         ),
                         vSpace,
-                        ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: model!.questions?.length ?? 0,
-                            itemBuilder: (context, index) {
-                              controller.selectedAnswerList.add();
-                              return getQuestionQuizWidget(
-                                  model!.questions![index],
-                                  controller.selectedAnswerList[index]);
-                            }),
-                        vSpace,
-                        Button(
-                          buttonText: 'Submit',
-                          onTap: () {
-                            controller.calculateScores();
-                          },
-                        ),
+
+                        !controller.isAnswerSubmitted.value
+                            ? Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  ListView.builder(
+                                      physics:
+                                          const NeverScrollableScrollPhysics(),
+                                      shrinkWrap: true,
+                                      itemCount: model!.questions?.length ?? 0,
+                                      itemBuilder: (context, index) {
+                                        ///correct answer: selected answer
+                                        controller.selectedAnswerList
+                                            .add(''.obs);
+                                        return getQuestionQuizWidget(
+                                            model!.questions![index],
+                                            controller
+                                                .selectedAnswerList[index]);
+                                      }),
+                                  vSpace,
+                                  Button(
+                                    buttonText: 'Submit',
+                                    leftPadding: 50.w,
+                                    rightPading: 50.w,
+                                    textColor: AppColor.whiteColor,
+                                    onTap: () {
+                                      if (model != null) {
+                                        controller.isAnswerSubmitted.value =
+                                            true;
+                                        controller.calculateScores(model!);
+                                      }
+                                    },
+                                  ),
+                                  vSpace,
+                                ],
+                              )
+                            : Column(
+                                /// answers building
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                      'Your scores are = ${controller.scores} / ${model?.questions?.length ?? 0}'),
+                                  vSpace,
+                                  Button(
+                                    buttonText: 'Reset',
+                                    leftPadding: 50.w,
+                                    rightPading: 50.w,
+                                    textColor: AppColor.whiteColor,
+                                    onTap: () {
+                                      controller.resetQuiz();
+                                    },
+                                  ),
+                                ],
+                              ),
+
                         vSpace,
                       ],
                     ),
